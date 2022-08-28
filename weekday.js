d4a8a826bbd5e73005c7fb48cfbc786b5911e728
varsha p <varshap630@gmail.com>
2022-08-28 13:47:49 +0530

    display the weekday

---
+++ weekday.js
@@ -0,0 +1,16 @@
+if(new Date().getDay()){
+    day="Saturday";
+}else if(new Date().getDay()){
+    day="Sunday";
+}else if(new Date().getDay()){
+    day="Monday";
+}else if(new Date().getDay()){
+    day="Tuesday";
+}else if(new Date().getDay()){
+    day="Wesnesday";
+}else if(new Date().getDay()){
+    day="Thursday";
+}else{
+    day="Friday";
+}
+console.log(day);
