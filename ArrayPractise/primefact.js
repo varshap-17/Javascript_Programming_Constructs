9b2a75166c588c003143ba59ac3efdf8c73b0532
varsha p <varshap630@gmail.com>
2022-08-28 22:01:06 +0530

    prime factorization to store prime factors

---
+++ ArrayPractise/primefact.js
@@ -0,0 +1,14 @@
+function primeFactors(n){
+    var factors=[];
+    divisor=2;
+    while(n>2){
+        if(n%divisor==0){
+            factors.push(divisor);
+            n=n/divisor;
+        }else{
+            divisor++;
+        }
+    }
+    return factors;
+}
+console.log(primeFactors(36));
