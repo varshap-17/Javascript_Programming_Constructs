03a7423a2ab8c9fa0cd723cc8607146b15c70344
varsha p <varshap630@gmail.com>
2022-08-28 13:28:45 +0530

    program to check leapyear

---
+++ leapyear.js
@@ -0,0 +1,9 @@
+function isLeapYear(year){
+    if(year %4 ==0){
+        console.log("leap year");
+    }else{
+        console.log("not a leap year");
+    }
+}
+var myYear=2020;
+isLeapYear(myYear)
